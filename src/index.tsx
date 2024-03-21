b24b03b674e06573dfedb8b5712789d63dd8b99f
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-03-21 15:15:47 -0700

    [feat] imported tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "./index.css";
+
 const root = document.createElement("div");
 root.className = "container";
 document.body.appendChild(root);
