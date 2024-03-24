a1e42e61bfe7422bb1bfa038529142e08d213a4e
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-03-24 15:36:00 -0700

    [feat] updated button paths

--- src/routes/MainInterface.tsx
+++ src/routes/MainInterface.tsx
@@ -122,7 +122,7 @@ const MainInterface = () => {
 			<div className="setting">
 				<strong className="text-xlg">Settings</strong>
 				<div className=" flex justify-center mt-4 mb-6 space-x-2">
-					<a href="#_" className="relative inline-block text-lg group">
+					<Link to="/languages" className="relative inline-block text-lg group">
 						<span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
 							<span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
 							<span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
@@ -132,9 +132,9 @@ const MainInterface = () => {
 							className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
 							data-rounded="rounded-lg"
 						></span>
-					</a>
+					</Link>
 
-					<a href="#_" className="relative inline-block text-lg group">
+					<Link to="/configure" className="relative inline-block text-lg group">
 						<span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
 							<span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
 							<span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
@@ -144,7 +144,7 @@ const MainInterface = () => {
 							className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
 							data-rounded="rounded-lg"
 						></span>
-					</a>
+					</Link>
 				</div>
 
 				<Link to="/support" className="text-blue-500 hover:text-blue-800">
