11ca53f1f40d7d3c27ca32403c308e6c107c1ce6
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-03-24 15:35:38 -0700

    [feat] added routes to new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import MainInterface from "./routes/MainInterface";
+import Configure from "./routes/Configure";
+import Languages from "./routes/Languages";
 
 function App() {
 	return (
@@ -7,6 +9,8 @@ function App() {
 			<Routes>
 				{/* <Route path="/login" element={<LoginPage />} /> */}
 				<Route path="/" element={<MainInterface />} />
+				<Route path="/configure" element={<Configure />} />
+				<Route path="/languages" element={<Languages />} />
 				{/* Other routes can go here */}
 			</Routes>
 		</HashRouter>
