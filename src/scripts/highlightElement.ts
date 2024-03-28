419441e19049a42c0caff745c0ab1e908fb5c0c3
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-03-28 11:38:30 -0700

    [fix] removed get element by id

--- src/scripts/highlightElement.ts
+++ src/scripts/highlightElement.ts
@@ -1,26 +1,25 @@
 export const highlightElements = () => {
 	console.log("Button clicked! This is where your logic goes.");
-	document
-		.getElementById("highlightButton")
-		?.addEventListener("click", function () {
-			console.log("highlight button clicked !");
-			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-				if (tabs[0]?.id) {
-					chrome.scripting
-						.executeScript({
-							target: { tabId: tabs[0].id },
-							files: ["js/content.js"],
-						})
-						.then(() => console.log("injected script file"));
 
-					chrome.tabs.sendMessage(
-						tabs[0].id,
-						{ toggle: true },
-						function (response) {
-							console.log("Toggle message sent to content script.");
-						},
-					);
-				}
-			});
+	document?.addEventListener("click", function () {
+		console.log("highlight button clicked!");
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			if (tabs[0]?.id) {
+				chrome.scripting
+					.executeScript({
+						target: { tabId: tabs[0].id },
+						files: ["js/content.js"],
+					})
+					.then(() => console.log("injected script file"));
+
+				chrome.tabs.sendMessage(
+					tabs[0].id,
+					{ toggle: true },
+					function (response) {
+						console.log("Toggle message sent to content script.");
+					},
+				);
+			}
 		});
+	});
 };
