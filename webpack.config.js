fdefebd563530ec465a71c6f3b448e32ebb7ffb0
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-03-27 20:55:22 -0700

    [feat] updated content script entry

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
 	entry: {
 		index: "./src/index.tsx",
+		content: "./src/scripts/content.ts",
 	},
 	cache: false,
 	mode: "production",
