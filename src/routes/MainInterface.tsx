76803309d79b04b2bcc8953569053f8a79b077f3
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-12 13:47:24 -0700

    [refactor] added the import directly from the content script

--- src/routes/MainInterface.tsx
+++ src/routes/MainInterface.tsx
@@ -2,9 +2,13 @@ import { Link } from "react-router-dom";
 
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 
-import { highlightElements } from "../scripts/highlightElement";
+import { requestScreenshot } from "../scripts/screenshotContent";
 
 const MainInterface = () => {
+	const handleScreenshot = () => {
+		requestScreenshot();
+	};
+
 	return (
 		// TODO: split the list and larger divs into seperate components
 		<div className="bg-white rounded px-14 pt-4 pb-8 mb-4">
@@ -19,12 +23,12 @@ const MainInterface = () => {
 				<div className=" flex justify-center mb-6 space-x-2">
 					<button
 						className="relative inline-block text-lg group"
-						onClick={highlightElements}
+						onClick={handleScreenshot}
 					>
 						<span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
 							<span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
 							<span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-							<span className="relative">Add Comments</span>
+							<span className="relative">Take Screenshot</span>
 						</span>
 						<span
 							className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
