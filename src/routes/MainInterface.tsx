988152a5742d2ff34ad6afd8773d342025257cf5
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-13 16:06:19 -0700

    [refactor] updated desc

--- src/routes/MainInterface.tsx
+++ src/routes/MainInterface.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 
-import { requestScreenshot } from "../scripts/screenshotContent";
+import { requestScreenshot } from "../scripts/content";
 
 const MainInterface = () => {
 	const handleScreenshot = () => {
@@ -15,7 +15,7 @@ const MainInterface = () => {
 			<div className="main-header">
 				<div className="text-3xl font-bold mb-4">CommentCast</div>
 				<div className="mb-4">
-					<p>Leave comments directly on your favourite app!</p>
+					<p>Create reproducible issues and share them! </p>
 				</div>
 			</div>
 
@@ -53,12 +53,11 @@ const MainInterface = () => {
 			<div className="setting">
 				<strong className="text-xlg">Settings</strong>
 				<div className=" flex justify-center mt-4 mb-6 space-x-2">
-					{/* TODO: Make into a switch that cuts on and off the comments */}
 					<Link to="/" className="relative inline-block text-lg group">
 						<span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
 							<span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
 							<span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-							<span className="relative">Show Comments</span>
+							<span className="relative">Configuration</span>
 						</span>
 						<span
 							className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
