aa0ef43232a1308e647457a2d1576ab29e619868
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-15 13:56:23 -0700

    [feat] updated background with user import

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,3 +1,7 @@
 import { screenshotBackground } from "./background-scripts/screenshotBackground";
+import { getAuthCookie } from "./background-scripts/authBackground";
 
+getAuthCookie((cookie) => {
+	console.log("Auth cookie:", cookie);
+});
 screenshotBackground();
