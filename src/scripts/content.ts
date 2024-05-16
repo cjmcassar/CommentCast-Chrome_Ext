b097b0da0eed1108872f90a098927ce095bcd7fd
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-15 18:58:30 -0700

    [refactor] removed screenshot update function

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,6 +1,3 @@
 import { requestScreenshot } from "./content-scripts/screenshotContent";
-import { handleScreenshotUpdate } from "./content-scripts/screenshotContent";
-
-handleScreenshotUpdate();
 
 export { requestScreenshot };
