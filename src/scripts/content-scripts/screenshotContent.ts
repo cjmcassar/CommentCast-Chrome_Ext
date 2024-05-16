2a91e55e3cd405e03074b60503aa082605f7d1bc
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-15 18:59:02 -0700

    [fix] fixed bug to handle promise correctly

--- src/scripts/content-scripts/screenshotContent.ts
+++ src/scripts/content-scripts/screenshotContent.ts
@@ -1,42 +1,21 @@
-// Function to request a screenshot from the background script
-export function requestScreenshot() {
-	console.log("requestScreenshot");
+export async function requestScreenshot() {
+	console.log("requestScreenshot initiated");
 	try {
-		chrome.runtime.sendMessage({ msg: "take_screenshot" }, function (response) {
-			if (response && response.dataUrl) {
-				console.log("Screenshot URL received:", response.dataUrl);
-				downloadScreenshot(response.dataUrl);
-			}
+		const response = await new Promise((resolve, reject) => {
+			chrome.runtime.sendMessage(
+				{ msg: "take_screenshot" },
+				function (response) {
+					if (response && response.status === "Success") {
+						resolve(response.status);
+					} else {
+						reject(
+							new Error(response?.error || "Failed to receive screenshot URL"),
+						);
+					}
+				},
+			);
 		});
 	} catch (error) {
 		console.error("Failed to send message for screenshot:", error);
 	}
-}
-
-// Remove the below once we have the background script sending the data to the server
-
-// Function to download the screenshot using the data URL
-function downloadScreenshot(dataUrl: string) {
-	console.log("downloadScreenshot");
-	const link = document.createElement("a");
-	link.href = dataUrl;
-	link.download = "screenshot.png";
-	document.body.appendChild(link);
-	link.click();
-	document.body.removeChild(link);
-	console.log("Screenshot downloaded.");
-}
-
-export function handleScreenshotUpdate() {
-	chrome.runtime.onMessage.addListener(function (
-		message,
-		sender,
-		sendResponse,
-	) {
-		if (message.msg === "update_screenshot") {
-			console.log("Screenshot URL received:", message);
-			downloadScreenshot(message.imgUrl64);
-		}
-		sendResponse({ success: true });
-	});
 }
