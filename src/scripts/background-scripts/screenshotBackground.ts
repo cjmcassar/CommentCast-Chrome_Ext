cf536bdc69965a5ceb6e85db385c1f6792a93382
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-15 18:59:26 -0700

    [feat] added all types of shit that needs to be refactored

--- src/scripts/background-scripts/screenshotBackground.ts
+++ src/scripts/background-scripts/screenshotBackground.ts
@@ -1,107 +1,217 @@
 //chrome
 let isListenerAdded = false;
+let currentTabId: number | null = null;
+let isCaptureInProgress = false;
+
 export function screenshotBackground() {
 	chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-		console.log("background listener added");
+		console.log("tab id for tabId", tabId);
+		console.log("tab id for tab.id", tab.id);
+		console.log("changeInfo", changeInfo);
 		if (!isListenerAdded) {
+			console.log(
+				`adding listener for tab id ${currentTabId} = tab id ${tab.id}`,
+			);
 			chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-				messageListener(req, sendResponse, tab);
+				handleIssueRequest(req, sendResponse, tab);
+				return true;
 			});
+
 			isListenerAdded = true;
+			currentTabId = tabId;
+		} else if (
+			isListenerAdded &&
+			currentTabId !== tabId &&
+			changeInfo.status === "loading"
+		) {
+			console.log(
+				`tab id = ${currentTabId} changed to ${tabId}, removing old listener and adding a new one`,
+			);
+			chrome.runtime.onMessage.removeListener((req, sender, sendResponse) => {
+				console.log("removing listener for tab id", currentTabId);
+			});
+			chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+				handleIssueRequest(req, sendResponse, tab);
+				return true;
+			});
+			currentTabId = tabId;
 		}
 	});
 }
 
-const takeShot = async (windowId: number) => {
+const takeShot = async (windowId: number): Promise<string> => {
+	if (isCaptureInProgress) {
+		throw new Error(
+			"Screenshot capture is already in progress. Please try again later.",
+		);
+	}
+	isCaptureInProgress = true;
+
 	try {
 		let imgUrl64 = await chrome.tabs.captureVisibleTab(windowId, {
 			format: "jpeg",
 			quality: 80,
 		});
-		// let imgUrl64 = "test";
-		console.log(imgUrl64);
-		chrome.runtime.sendMessage({
-			msg: "update_screenshot",
-			imgUrl64: imgUrl64,
-		});
+
+		return imgUrl64;
 	} catch (error) {
 		console.error(error);
-		return error;
+		throw error;
+	} finally {
+		isCaptureInProgress = false;
 	}
 };
 
-const getConsoleLogs = async () => {
-	// Query for the active tab in the current window
-	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-		if (tabs.length > 0) {
-			const currentTab = tabs[0];
-			console.log("Current tab ID:", currentTab.id);
+const getConsoleLogs = async (): Promise<any[]> => {
+	return new Promise((resolve, reject) => {
+		// Query for the active tab in the current window
+		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+			if (tabs.length > 0) {
+				console.log("Current tab ID:", currentTabId);
 
-			// Proceed with getting console logs if a valid tab is found
-			if (typeof currentTab.id === "number") {
-				chrome.tabs.get(currentTab.id, (tab) => {
-					if (chrome.runtime.lastError) {
-						console.error(
-							`No tab with id: ${currentTab.id}`,
-							chrome.runtime.lastError.message,
-						);
-						return;
-					}
-
-					chrome.debugger.attach({ tabId: currentTab.id }, "1.0", function () {
+				// Proceed with getting console logs if a valid tab is found
+				if (typeof currentTabId === "number") {
+					chrome.tabs.get(currentTabId, (tab) => {
 						if (chrome.runtime.lastError) {
 							console.error(
-								`Failed to attach debugger to tab: ${currentTab.id}`,
+								`No tab with id: ${currentTabId}`,
 								chrome.runtime.lastError.message,
 							);
-							return;
+							return reject(chrome.runtime.lastError);
 						}
-						console.log("Debugger attached successfully.");
 
-						chrome.debugger.sendCommand(
-							{ tabId: currentTab.id },
-							"Console.enable",
-							{},
-							() => {
+						chrome.debugger.attach(
+							{ tabId: currentTabId ?? 0 },
+							"1.0",
+							function () {
 								if (chrome.runtime.lastError) {
 									console.error(
-										"Failed to enable console for tab:",
+										`Failed to attach debugger to tab: ${currentTabId}`,
 										chrome.runtime.lastError.message,
 									);
-									return;
+									return reject(chrome.runtime.lastError);
 								}
-								console.log("Console enabled for tab:", currentTab.id);
-							},
-						);
+								console.log("Debugger attached successfully.");
 
-						chrome.debugger.onEvent.addListener(
-							(debuggeeId, message, params) => {
-								// this is where the console log data will be sent to the server
-								console.log(
-									`Debugger event received: ${message} with params:`,
-									params,
-									`from debuggee ID: ${debuggeeId}`,
+								chrome.debugger.sendCommand(
+									{ tabId: currentTabId ?? 0 },
+									"Console.enable",
+									{},
+									() => {
+										if (chrome.runtime.lastError) {
+											console.error(
+												"Failed to enable console for tab:",
+												chrome.runtime.lastError.message,
+											);
+											return reject(chrome.runtime.lastError);
+										}
+										console.log("Console enabled for tab:", currentTabId);
+									},
 								);
 
-								chrome.debugger.detach({ tabId: currentTab.id }, function () {
-									console.log("Debugger detached from tab: " + currentTab.id);
-								});
+								const logs: any[] = [];
+								chrome.debugger.onEvent.addListener(
+									(debuggeeId, message, params) => {
+										if (
+											message === "Console.messageAdded" &&
+											params &&
+											(params as any).message
+										) {
+											logs.push((params as any).message);
+										}
+										console.log(
+											`Debugger event received: ${message} with params:`,
+											params,
+											`from debuggee ID: ${debuggeeId}`,
+										);
+									},
+								);
+								setTimeout(() => {
+									chrome.debugger.detach(
+										{ tabId: currentTabId ?? 0 },
+										function () {
+											if (chrome.runtime.lastError) {
+												console.error(
+													"Failed to detach debugger from tab:",
+													chrome.runtime.lastError.message,
+												);
+												return reject(chrome.runtime.lastError);
+											}
+											console.log(
+												"Debugger detached from tab: " + currentTabId,
+											);
+											resolve(logs);
+										},
+									);
+								}, 2000);
 							},
 						);
 					});
-				});
+				}
 			} else {
-				console.log("No active tab found in the current window.");
+				reject(new Error("No active tab found"));
 			}
-		} else {
-			console.log("No active tab found in the current window.");
-		}
+		});
 	});
+};
+
+const getPlatformInfo = async (): Promise<{
+	platformInfo: chrome.runtime.PlatformInfo;
+}> => {
+	return new Promise((resolve, reject) => {
+		chrome.runtime.getPlatformInfo((platformInfo) => {
+			if (chrome.runtime.lastError) {
+				console.error(
+					"Failed to get platform info:",
+					chrome.runtime.lastError.message,
+				);
+				return reject(chrome.runtime.lastError);
+			} else {
+				resolve({ platformInfo: platformInfo });
+			}
+		});
+	});
+};
+
+const getWindowSize = async (): Promise<{
+	windowSize: chrome.windows.Window;
+}> => {
+	return new Promise((resolve, reject) => {
+		chrome.windows.getCurrent((window) => {
+			if (chrome.runtime.lastError) {
+				console.error(
+					"Failed to get window size:",
+					chrome.runtime.lastError.message,
+				);
+				return reject(chrome.runtime.lastError);
+			}
+			resolve({ windowSize: window });
+		});
+	});
+};
+
+const getCountry = async (): Promise<string> => {
+	try {
+		const response = await fetch("https://ipapi.co/json/");
+		const data = await response.json();
+		return `${data.country_name} (${data.country_code})`;
+	} catch (error) {
+		console.error("Failed to get country information:", error);
+		return "Unknown";
+	}
+};
+
+const getScreenDimensions = (): { width: number; height: number } => {
+	return { width: window.screen.width, height: window.screen.height };
+};
+
+const getTimestamp = (): string => {
+	return new Date().toLocaleString();
 };
 
 // TODO: create function that gets browser information as well
 
-const messageListener = async (
+const handleIssueRequest = async (
 	req: { msg: string },
 	sendResponse: {
 		(response?: any): void;
@@ -109,19 +219,42 @@ const messageListener = async (
 	},
 	tab: chrome.tabs.Tab,
 ) => {
-	if (req.msg === "take_screenshot" && tab.windowId) {
-		// console.log("take_screenshot");
-		takeShot(tab.windowId)
-			.then(() => {
-				getConsoleLogs();
-				sendResponse({ status: "Screenshot taken" });
-			})
-			.catch((error) => {
-				console.error("Failed to take screenshot:", error);
-				sendResponse({
-					status: "Error taking screenshot",
-					error: error.toString(),
-				});
+	if (req.msg === "take_screenshot") {
+		try {
+			const [screenshot, logs, platformInfo, windowSize] = await Promise.all([
+				takeShot(tab.windowId),
+				getConsoleLogs(), // Ensure currentTabId is not null
+				getPlatformInfo(),
+				getWindowSize(),
+			]);
+			const response = {
+				status: "Success",
+				screenshot,
+				logs,
+				platformInfo,
+				windowSize,
+			};
+			console.log("Response from handleIssueRequest:", response);
+			sendResponse({
+				status: "Success",
+				screenshot,
+				logs,
+				platformInfo,
+				windowSize,
 			});
+		} catch (error) {
+			console.error("Failed to take screenshot or get logs:", error);
+			if (error instanceof Error) {
+				sendResponse({
+					status: "Error",
+					error: error.message,
+				});
+			} else {
+				sendResponse({
+					status: "Error",
+					error: "An unknown error occurred",
+				});
+			}
+		}
 	}
 };
