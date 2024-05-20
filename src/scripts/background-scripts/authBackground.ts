5dfe0c224a746f62770b207453092b4819d24829
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-20 15:30:24 -0700

    [feat] added getUser function to grab user info from local storage

--- src/scripts/background-scripts/authBackground.ts
+++ src/scripts/background-scripts/authBackground.ts
@@ -17,48 +17,102 @@ interface User {
 	};
 }
 
-export async function signInAnonymously() {
-	getAuthCookie(async (cookie) => {
-		if (cookie) {
-			handleExistingCookie(cookie);
-		} else {
-			console.log("No auth cookie found, signing in anonymously.");
+export function getUser(): Promise<User | undefined> {
+	return new Promise((resolve, reject) => {
+		chrome.storage.local.get(
+			["comment-cast-user-session", "comment-cast-anon-session"],
+			(result) => {
+				if (chrome.runtime.lastError) {
+					console.error(
+						"Failed to get user from local storage:",
+						chrome.runtime.lastError,
+					);
+					return reject(undefined);
+				}
+
+				const userSession = result["comment-cast-user-session"];
+				const anonSession = result["comment-cast-anon-session"];
+				if (userSession) {
+					try {
+						const user = userSession;
+						resolve({ user });
+					} catch (error) {
+						console.error("Failed to parse user session:", error);
+						reject(undefined);
+					}
+				} else if (anonSession) {
+					try {
+						const user = anonSession;
+						resolve({ user });
+					} catch (error) {
+						console.error("Failed to parse anonymous session:", error);
+						reject(undefined);
+					}
+				} else {
+					console.error("No user or anonymous session found in local storage");
+					resolve(undefined);
+				}
+			},
+		);
+	});
+}
+
+export async function signIn(): Promise<User> {
+	return new Promise(async (resolve) => {
+		getAuthCookie(async (cookie) => {
+			if (cookie) {
+				handleExistingCookie(cookie);
+				resolve(handleExistingCookie(cookie));
+			} else {
+				console.log("No auth cookie found, signing in anonymously.");
+				await handleAnonymousSignIn();
+			}
+		});
+	});
+}
+
+async function handleExistingCookie(
+	cookie: chrome.cookies.Cookie,
+): Promise<User> {
+	return new Promise(async (resolve) => {
+		console.log("Auth cookie found:", cookie.value);
+		try {
+			const decodedCookie = decodeURIComponent(cookie.value);
+			const cookieData = JSON.parse(decodedCookie);
+			const accessToken = cookieData.access_token;
+
+			const { data: user, error: userError } = await supabase.auth.getUser(
+				accessToken,
+			);
+			if (user.user && !userError) {
+				await handleUserAlreadySignedIn(user, cookieData);
+				resolve(user);
+			} else {
+				console.log(
+					"Auth cookie is invalid, signing in anonymously:",
+					userError,
+				);
+				await handleAnonymousSignIn();
+			}
+		} catch (error) {
+			console.error("Error parsing auth cookie:", error);
 			await handleAnonymousSignIn();
 		}
 	});
 }
 
-async function handleExistingCookie(cookie: chrome.cookies.Cookie) {
-	console.log("Auth cookie found:", cookie.value);
-	try {
-		const decodedCookie = decodeURIComponent(cookie.value);
-		const cookieData = JSON.parse(decodedCookie);
-		const accessToken = cookieData.access_token;
+function handleUserAlreadySignedIn(user: User, cookieData: any) {
+	return new Promise(async (resolve) => {
+		console.log("User is already signed in:", user);
 
-		const { data: user, error: userError } = await supabase.auth.getUser(
-			accessToken,
-		);
-		if (user.user && !userError) {
-			await handleUserAlreadySignedIn(user, cookieData);
+		const existingUserSession = await getExistingUserSession();
+		if (!existingUserSession) {
+			storeUserSession(cookieData);
 		} else {
-			console.log("Auth cookie is invalid, signing in anonymously:", userError);
-			await handleAnonymousSignIn();
+			console.log("User session already exists.");
+			resolve(user);
 		}
-	} catch (error) {
-		console.error("Error parsing auth cookie:", error);
-		await handleAnonymousSignIn();
-	}
-}
-
-async function handleUserAlreadySignedIn(user: User, cookieData: any) {
-	console.log("User is already signed in:", user);
-
-	const existingUserSession = await getExistingUserSession();
-	if (!existingUserSession) {
-		storeUserSession(cookieData);
-	} else {
-		console.log("User session already exists.");
-	}
+	});
 }
 
 async function getExistingUserSession() {
