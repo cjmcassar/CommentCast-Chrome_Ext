7f494df620f17b6a1bed1a250f5eb16e277f8b16
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-19 19:02:14 -0700

    [feat] added notificationlink function to background script

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,7 +1,9 @@
 import { screenshotBackground } from "./background-scripts/screenshotBackground";
 import { getAuthCookie } from "./background-scripts/authBackground";
+import notificationLink from "./background-scripts/notificationsBackground";
 
 getAuthCookie((cookie) => {
 	console.log("Auth cookie:", cookie);
 });
 screenshotBackground();
+notificationLink();
