bd512cf83b151df41e0472d2cca9e372ec045dc6
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-19 19:02:46 -0700

    [feat] created notification once data is added to supabase

--- src/scripts/content-scripts/screenshotContent.ts
+++ src/scripts/content-scripts/screenshotContent.ts
@@ -7,6 +7,34 @@ export async function requestScreenshot() {
 				function (response) {
 					if (response && response.status === "Success") {
 						resolve(response.status);
+
+						const issueUrl = `http://localhost:3000/issues/${response.id}`;
+
+						//TODO: the notification is created but the condition maybe is too strick
+						// so it won't up after the first screenshot
+						chrome.notifications.create(
+							{
+								type: "basic",
+								iconUrl: "/public/logo192.png", // Path to your notification icon
+								title: "Screenshot Taken",
+								message: "The screenshot was taken successfully!",
+								isClickable: true,
+							},
+							function (notificationId) {
+								if (chrome.runtime.lastError) {
+									console.error(
+										"Notification error:",
+										chrome.runtime.lastError,
+									);
+								} else {
+									console.log("Notification created with ID:", notificationId);
+									chrome.storage.local.set({ [notificationId]: issueUrl });
+								}
+							},
+						);
+						console.log("response", response);
+
+						return response;
 					} else {
 						reject(
 							new Error(response?.error || "Failed to receive screenshot URL"),
