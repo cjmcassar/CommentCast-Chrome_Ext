088b61201d967c8cccb2d84d8e7208097b653317
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-21 16:10:14 -0700

    [refactor] added https request for database inserts

--- src/scripts/background-scripts/screenshotBackground.ts
+++ src/scripts/background-scripts/screenshotBackground.ts
@@ -1,22 +1,28 @@
-import { supabase } from "../utils/supabase/supabaseClient";
-import { getUser } from "./authBackground";
+import { getUser, getSession } from "./authBackground";
+import { getPlatformInfo } from "./screenshot-utils/OSInfoBackground";
+import { getBrowserName } from "./screenshot-utils/browserNameBackground";
+import { getCurrentTabUrl } from "./screenshot-utils/currentTabUrlBackground";
+import { getConsoleLogs } from "./screenshot-utils/debugLogsBackground";
+import { getScreenDimensions } from "./screenshot-utils/screenDimensions";
+import { takeScreenshot } from "./screenshot-utils/takeScreenshotBackground";
 
 let isListenerAdded = false;
 let currentTabId: number | null = null;
-let isCaptureInProgress = false;
 
 // todo: Add the following information:
 
 // Country
 // Screen Dimensions
 
-// todo: add authentication via the authBackground script
+interface Session {
+	session: {
+		access_token: string;
+	};
+}
 
 interface User {
 	user: {
-		user: {
-			id: string;
-		};
+		id: string;
 	};
 }
 
@@ -56,195 +62,6 @@ export function screenshotBackground() {
 	});
 }
 
-const takeScreenshot = async (windowId: number): Promise<string> => {
-	if (isCaptureInProgress) {
-		console.log(
-			"Screenshot capture is already in progress. Please try again later.",
-		);
-		return "";
-	}
-	isCaptureInProgress = true;
-
-	try {
-		let imgUrl64 = await chrome.tabs.captureVisibleTab(windowId, {
-			format: "jpeg",
-			quality: 80,
-		});
-
-		return imgUrl64;
-	} catch (error) {
-		console.error(error);
-		throw error;
-	} finally {
-		isCaptureInProgress = false;
-	}
-};
-
-const getConsoleLogs = async (): Promise<any[]> => {
-	return new Promise((resolve, reject) => {
-		// Query for the active tab in the current window
-		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-			if (tabs.length > 0) {
-				console.log("Current tab ID:", currentTabId);
-
-				// Proceed with getting console logs if a valid tab is found
-				if (typeof currentTabId === "number") {
-					chrome.tabs.get(currentTabId, (tab) => {
-						if (chrome.runtime.lastError) {
-							console.error(
-								`No tab with id: ${currentTabId}`,
-								chrome.runtime.lastError.message,
-							);
-							return reject(chrome.runtime.lastError);
-						}
-
-						chrome.debugger.attach(
-							{ tabId: currentTabId ?? 0 },
-							"1.0",
-							function () {
-								if (chrome.runtime.lastError) {
-									console.log(
-										`Failed to attach debugger to tab: ${currentTabId}`,
-										chrome.runtime.lastError.message,
-									);
-									return reject(chrome.runtime.lastError);
-								}
-								console.log("Debugger attached successfully.");
-
-								chrome.debugger.sendCommand(
-									{ tabId: currentTabId ?? 0 },
-									"Console.enable",
-									{},
-									() => {
-										if (chrome.runtime.lastError) {
-											console.error(
-												"Failed to enable console for tab:",
-												chrome.runtime.lastError.message,
-											);
-											return reject(chrome.runtime.lastError);
-										}
-										console.log("Console enabled for tab:", currentTabId);
-									},
-								);
-
-								const logs: any[] = [];
-								chrome.debugger.onEvent.addListener(
-									(debuggeeId, message, params) => {
-										if (
-											message === "Console.messageAdded" &&
-											params &&
-											(params as any).message
-										) {
-											logs.push((params as any).message);
-										}
-										console.log(
-											`Debugger event received: ${message} with params:`,
-											params,
-											`from debuggee ID: ${debuggeeId}`,
-										);
-									},
-								);
-								setTimeout(() => {
-									chrome.debugger.detach(
-										{ tabId: currentTabId ?? 0 },
-										function () {
-											if (chrome.runtime.lastError) {
-												console.error(
-													"Failed to detach debugger from tab:",
-													chrome.runtime.lastError.message,
-												);
-												return reject(chrome.runtime.lastError);
-											}
-											console.log(
-												"Debugger detached from tab: " + currentTabId,
-											);
-											resolve(logs);
-										},
-									);
-								}, 2000);
-							},
-						);
-					});
-				}
-			} else {
-				reject(new Error("No active tab found"));
-			}
-		});
-	});
-};
-
-const getPlatformInfo = async (): Promise<{
-	platformInfo: chrome.runtime.PlatformInfo;
-}> => {
-	return new Promise((resolve, reject) => {
-		chrome.runtime.getPlatformInfo((platformInfo) => {
-			if (chrome.runtime.lastError) {
-				console.error(
-					"Failed to get platform info:",
-					chrome.runtime.lastError.message,
-				);
-				return reject(chrome.runtime.lastError);
-			} else {
-				resolve({ platformInfo: platformInfo });
-			}
-		});
-	});
-};
-
-const getCurrentTabUrl = async (): Promise<string> => {
-	return new Promise((resolve, reject) => {
-		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-			if (tabs.length > 0 && tabs[0].url) {
-				resolve(tabs[0].url);
-			} else {
-				reject(new Error("No active tab found or URL is not accessible."));
-			}
-		});
-	});
-};
-
-const getScreenDimensions = async (): Promise<{
-	width: number;
-	height: number;
-}> => {
-	return new Promise((resolve, reject) => {
-		chrome.system.display.getInfo((displays) => {
-			if (displays.length > 0) {
-				// This gets the dimensions of the primary display
-				// todo: add support for multiple displays
-				// todo: add support for different window sizes
-				const primaryDisplay = displays.find((display) => display.isPrimary);
-				if (primaryDisplay) {
-					resolve({
-						width: primaryDisplay.bounds.width,
-						height: primaryDisplay.bounds.height,
-					});
-				} else {
-					reject(new Error("Primary display not found."));
-				}
-			} else {
-				reject(new Error("No displays found."));
-			}
-		});
-	});
-};
-
-const getBrowserName = (): string => {
-	const userAgent = navigator.userAgent;
-	switch (true) {
-		case userAgent.includes("Chrome"):
-			return "Google Chrome";
-		case userAgent.includes("Firefox"):
-			return "Mozilla Firefox";
-		case userAgent.includes("Safari"):
-			return "Apple Safari";
-		case userAgent.includes("Edge"):
-			return "Microsoft Edge";
-		default:
-			return "Unknown Browser";
-	}
-};
-
 const handleIssueRequest = async (
 	req: { msg: string },
 	sendResponse: {
@@ -255,9 +72,8 @@ const handleIssueRequest = async (
 ) => {
 	if (req.msg === "take_screenshot") {
 		try {
-			// Get the user ID
-
 			const [
+				session,
 				user,
 				screenshot,
 				logs,
@@ -266,6 +82,7 @@ const handleIssueRequest = async (
 				browserName,
 				primaryDisplayDimensions,
 			] = await Promise.all([
+				getSession() as Promise<Session | undefined>,
 				getUser() as Promise<User | undefined>,
 				takeScreenshot(tab.windowId),
 				getConsoleLogs(),
@@ -275,6 +92,7 @@ const handleIssueRequest = async (
 				getScreenDimensions(),
 			]);
 			const response = {
+				session,
 				user,
 				status: "Success",
 				screenshot,
@@ -285,11 +103,22 @@ const handleIssueRequest = async (
 				primaryDisplayDimensions,
 			};
 
-			const { data, error } = await supabase
-				.from("issue_snapshots")
-				.insert([
-					{
-						uuid: response.user?.user.user.id,
+			console.log("response", response);
+
+			const supabaseResponse = await fetch(
+				"https://yvdtkxnmiehnrwmjkjxp.supabase.co/rest/v1/issue_snapshots?select=id",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${response.session?.session.access_token}`,
+						apikey:
+							"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2ZHRreG5taWVobnJ3bWpranhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5NDEyMzIsImV4cCI6MjAyODUxNzIzMn0.g4dgWoHBDONdD6YNQ1SZ6PwL6nfj1L-F5TFq1vI3Fb4",
+						Prefer: "return=representation",
+					},
+
+					body: JSON.stringify({
+						uuid: response.user?.user.id,
 						screenshot: response.screenshot,
 						logs: response.logs,
 						platform_arch: response.platformInfo.platformInfo.arch,
@@ -300,17 +129,24 @@ const handleIssueRequest = async (
 							primary_display_width: response.primaryDisplayDimensions.width,
 							primary_display_height: response.primaryDisplayDimensions.height,
 						},
-					},
-				])
-				.select("id");
-			if (error) {
-				console.error("Failed to insert data into Supabase:", error);
-				sendResponse({ status: "Error", error: error.message });
+					}),
+				},
+			);
+
+			if (!supabaseResponse.ok) {
+				console.error(
+					"Failed to insert data into Supabase:",
+					await supabaseResponse.text(),
+				);
+				sendResponse({
+					status: "Error",
+					error: "Failed to insert data into Supabase",
+				});
 			} else {
-				// console.log("Inserted data into Supabase:", data);
-				const insertedId = data[0].id; // Get the ID of the inserted record
-				console.log("Inserted record ID:", response);
-				sendResponse({ status: "Success", id: insertedId });
+				console.log("Data inserted successfully into Supabase.");
+				const data = await supabaseResponse.json();
+				console.log("Data inserted successfully into Supabase:", data);
+				sendResponse({ status: "Success", id: data[0].id });
 			}
 		} catch (error) {
 			console.error("Failed to take screenshot or get logs:", error);
