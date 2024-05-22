d9dac36bd192677cdaa9fde53cb38803d36d31cc
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-22 11:14:39 -0700

    [refactor] removed utils

--- src/scripts/background-scripts/authBackground.ts
+++ src/scripts/background-scripts/authBackground.ts
@@ -1,48 +1,4 @@
-import { SupabaseClient, createClient } from "@supabase/supabase-js";
-
-// To fetch items from storage
-export const getLocalStorage = async (key: string): Promise<any> =>
-	(await chrome.storage.local.get(key))[key];
-
-// To remove storage key from the chrome storage
-export const removeLocalStorage = async (key: string): Promise<void> =>
-	await chrome.storage.local.remove(key);
-
-// For setting storage
-export const setLocalStorage = async (dataObject: any): Promise<void> =>
-	await chrome.storage.local.set(dataObject);
-
-const storageAdapter = {
-	getItem: async (name: string) => {
-		return await getLocalStorage(name);
-	},
-
-	setItem: async (name: string, value: string) => {
-		return await setLocalStorage({ [name]: value });
-	},
-
-	removeItem: async (name: string) => {
-		return await removeLocalStorage(name);
-	},
-};
-
-const options = {
-	auth: {
-		debug: false,
-		persistSession: true,
-		storage: storageAdapter,
-	},
-};
-
-const supabaseUrl = "https://yvdtkxnmiehnrwmjkjxp.supabase.co";
-const supabaseKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2ZHRreG5taWVobnJ3bWpranhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5NDEyMzIsImV4cCI6MjAyODUxNzIzMn0.g4dgWoHBDONdD6YNQ1SZ6PwL6nfj1L-F5TFq1vI3Fb4";
-
-const supabase: SupabaseClient = createClient(
-	supabaseUrl,
-	supabaseKey,
-	options,
-);
+import { appUrl, supabase } from "./auth-utils/utils";
 
 interface Session {
 	session: {
@@ -55,6 +11,8 @@ interface User {
 		id: string;
 	};
 }
+
+// TODO: when user isn't signed in then redirect them to the sign in page on website
 
 export async function getUser(): Promise<User | undefined> {
 	try {
@@ -71,11 +29,13 @@ export async function getUser(): Promise<User | undefined> {
 			);
 			if (userError) {
 				console.error("Error retrieving user:", userError);
+				chrome.tabs.create({ url: `${appUrl}/login` });
 				return undefined;
 			}
 			return user;
 		} else {
 			console.log("No active session found.");
+			chrome.tabs.create({ url: `${appUrl}/login` });
 			return undefined;
 		}
 	} catch (error) {
