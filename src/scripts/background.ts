5c7c46e8d1533aaec619f2ae5331c32dd6bc0fe0
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-23 12:17:09 -0700

    [refactor] updated import

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,6 +1,6 @@
 import { screenshotBackground } from "./background-scripts/screenshotBackground";
 import { signIn } from "./background-scripts/authBackground";
-import notificationLink from "./background-scripts/notificationsBackground";
+import notificationLink from "./background-scripts/browser-utils/notificationsBackground";
 
 signIn();
 
