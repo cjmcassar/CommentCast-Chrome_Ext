664ab20399707d93ff4228baf25d84b06dbc9da1
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-22 18:46:14 -0700

    [refactor] imported supabase insert func

--- src/scripts/background-scripts/screenshotBackground.ts
+++ src/scripts/background-scripts/screenshotBackground.ts
@@ -1,8 +1,9 @@
-import { getUser, getSession, getAuthCookie } from "./authBackground";
+import { getUser, getSession } from "./authBackground";
 import { getPlatformInfo } from "./screenshot-utils/OSInfoBackground";
 import { getBrowserName } from "./screenshot-utils/browserNameBackground";
 import { getCurrentTabUrl } from "./screenshot-utils/currentTabUrlBackground";
 import { getConsoleLogs } from "./screenshot-utils/debugLogsBackground";
+import { insertDBBackground } from "./screenshot-utils/insertDBBackground";
 import { getScreenDimensions } from "./screenshot-utils/screenDimensions";
 import { takeScreenshot } from "./screenshot-utils/takeScreenshotBackground";
 
@@ -29,20 +30,20 @@ interface User {
 export function screenshotBackground() {
 	if (!isListenerAdded) {
 		chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-			console.log("tab id for tabId", tabId);
-			console.log("tab id for tab.id", tab.id);
-			console.log("changeInfo", changeInfo);
+			// console.log("tab id for tabId", tabId);
+			// console.log("tab id for tab.id", tab.id);
+			// console.log("changeInfo", changeInfo);
 
 			if (currentTabId === null || currentTabId !== tabId) {
 				if (currentTabId !== null) {
 					console.log(
 						`tab id = ${currentTabId} changed to ${tabId}, removing old listener and adding a new one`,
 					);
-					// Remove the listener only if it was previously added
+
 					chrome.runtime.onMessage.removeListener(handleIssueRequest);
 				}
 
-				// Add the listener only once per tab update where conditions are met
+				// I can't add a return here else it will do this repeatedly
 				chrome.runtime.onMessage.addListener(handleIssueRequest);
 
 				currentTabId = tabId;
@@ -94,49 +95,10 @@ const handleIssueRequest = async (
 
 			console.log("response", response);
 
-			const supabaseResponse = await fetch(
-				"https://yvdtkxnmiehnrwmjkjxp.supabase.co/rest/v1/issue_snapshots?select=id",
-				{
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `Bearer ${response.session?.session.access_token}`,
-						apikey:
-							"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2ZHRreG5taWVobnJ3bWpranhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5NDEyMzIsImV4cCI6MjAyODUxNzIzMn0.g4dgWoHBDONdD6YNQ1SZ6PwL6nfj1L-F5TFq1vI3Fb4",
-						Prefer: "return=representation",
-					},
-
-					body: JSON.stringify({
-						uuid: response.user?.user.id,
-						screenshot: response.screenshot,
-						logs: response.logs,
-						platform_arch: response.platformInfo.platformInfo.arch,
-						platform_os: response.platformInfo.platformInfo.os,
-						url: response.url,
-						browser_name: response.browserName,
-						primary_display_dimensions: {
-							primary_display_width: response.primaryDisplayDimensions.width,
-							primary_display_height: response.primaryDisplayDimensions.height,
-						},
-					}),
-				},
-			);
-
-			if (!supabaseResponse.ok) {
-				console.error(
-					"Failed to insert data into Supabase:",
-					await supabaseResponse.text(),
-				);
-				sendResponse({
-					status: "Error",
-					error: "Failed to insert data into Supabase",
-				});
-			} else {
-				console.log("Data inserted successfully into Supabase.");
-				const data = await supabaseResponse.json();
-				console.log("Data inserted successfully into Supabase:", data);
-				sendResponse({ status: "Success", id: data[0].id });
-			}
+			insertDBBackground(response, (dbResponse) => {
+				console.log("Sending success response with DB data", dbResponse);
+				sendResponse(dbResponse);
+			});
 		} catch (error) {
 			console.error("Failed to take screenshot or get logs:", error);
 			if (error instanceof Error) {
@@ -151,7 +113,5 @@ const handleIssueRequest = async (
 				});
 			}
 		}
-		sendResponse({ status: "Success" });
-		return true; // Keep the listener active
 	}
 };
