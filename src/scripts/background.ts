629300a8ef9e7e84ce4b5c0dbfce97bf04d49be8
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-23 12:48:18 -0700

    [feat] add in check when user signs in

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -2,6 +2,23 @@ import { screenshotBackground } from "./background-scripts/screenshotBackground"
 import { signIn } from "./background-scripts/authBackground";
 import notificationLink from "./background-scripts/browser-utils/notificationsBackground";
 
+function checkUrlAndSignIn(
+	tabId: number,
+	changeInfo: chrome.tabs.TabChangeInfo,
+	tab: chrome.tabs.Tab,
+) {
+	console.log("checkUrlAndSignIn");
+	const url = tab.url;
+	if (
+		url &&
+		(url.includes("http://localhost:3000") || url.includes("commentcast.xyz"))
+	) {
+		signIn();
+	}
+}
+
+chrome.tabs.onUpdated.addListener(checkUrlAndSignIn);
+
 signIn();
 
 screenshotBackground();
